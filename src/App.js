3423de4115ea66d700ae69b0ac5d1de4a5cb7d2d
Sathyaraj <saridevan.c@stc.com.bh>
2023-01-17 09:46:59 +0300

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [textInput, setTextInput] = useState("");
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": "1c03575da1mshe5a1953660b645dp1f91d1jsn91c7d035905a",
+      "X-RapidAPI-Host": "you-chat-gpt.p.rapidapi.com",
+    },
+    body: JSON.stringify({ question: textInput, max_response_time: 30 }),
+  };
+
+  async function onSubmit(event) {
+    setLoading(true);
+    event.preventDefault();
+    await fetch("https://you-chat-gpt.p.rapidapi.com/", options)
+      .then((response) => response.json())
+      .then((response) => {
+        setResult(response);
+        console.log(response);
+      })
+      .catch((err) => console.error(err));
+    setLoading(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>ChatGPT</h2>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="search"
+            placeholder="Ask me anything..."
+            value={textInput}
+            onChange={(e) => setTextInput(e.target.value)}
+          />
+          <input type="submit" value="Generate answers" />
+        </form>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div
+            className="search-results"
+            dangerouslySetInnerHTML={{ __html: result.answer }}
+          ></div>
+        )}
+      </main>
     </div>
   );
 }
-
-export default App;
